7c975f05fd0d7578682e076ca85d7d602d432dc4
Marcos Maerli <maerli.pereira@gmail.com>
2021-07-03 16:03:40 -0300

    Update localhost.js

--- localhost.js
+++ localhost.js
@@ -1,2 +1,2 @@
-let file = location.hash.slice(2).split("-").slice(1).Join("-");
+let file = location.hash.slice(2).split("-").slice(1).join("-");
 location.href= "http://localhost:3000/prova#/prova-"+file;
