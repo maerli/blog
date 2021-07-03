f61cba629925fbc47ac953864f6d71a208dfc22f
Marcos Maerli <maerli.pereira@gmail.com>
2021-07-03 15:52:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ require("https://unpkg.com/react@17/umd/react.development.js");
 require("https://unpkg.com/react-dom@17/umd/react-dom.development.js", function(){
 
 var url = "https://cdn.jsdelivr.net/gh/maerli/blog@latest/";
-let file = location.hash.slice(2)
+let p = file.split("-");
 if(file === "") file = "index";
-require(url+file+".js")
+require(url+p[0]+".js");
 });
 
 
