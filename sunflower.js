3313f3860f5c4f3635b4318fd8506a45e53c43e9
Marcos Maerli <maerli.pereira@gmail.com>
2021-08-03 19:17:39 -0300

    Add files via upload

---
+++ sunflower.js
@@ -0,0 +1,87 @@
+const e = React.createElement;
+function p(txt){
+    return e("p",{style:{textAlign:"justify"}}, txt);
+}
+
+ function App(){
+return e("div", {style:{padding:50}} , 
+
+e("hr"),
+e("strong", null, "AUTORA: "),
+e("span", null, "STEFANY SILVA"),
+e("hr"),
+e("h1",null,"i met a sunflower"),
+e("h6",null, "eu conheci um girasol"),
+
+p(`Em um certo momento em minha vida,
+conheci uma pessoa que mudou
+praticamente tudo dentro de mim. E que
+quando o conheci, nem imaginava que ele
+seria tão importante para mim como é hoje..`),
+
+p(`Porém, para não ser muito invasivo...
+digamos que conheci um girassol.`),
+
+p(`Ele me chamou atenção desde a primeira
+vez que vi, pois parecia ser muito legal. E
+interessante também. Tentei me aproximar
+de si, e cada vez que o encontrava, fazia o
+mesmo.`),
+
+p(`Mas nunca imaginei que ele continuaria
+sendo presente em minha vida e
+principalmente, em meu coração.`),
+
+p(`Até que teve um dia que nos aproximamos para valer. Não vou mentir, não me arrependo desse acontecimento... Mas foi um pouco complicado no começo para raciocinar e me adaptar com tudo.`),
+
+p(`Via que minhas outras amigas também tinham seus girassóis, e que eles agiam de uma certa forma com elas, que meu girassol não agia comigo. Comecei a ficar preocupada e me questionar se meu girassol me queria mesmo... se gostava mesmo de mim.`),
+
+p(`Mesmo ele respondendo que sim, ainda não conseguia acreditar. Mas, não era apenas pelo fato de ver a situação de minhas amigas ou algo de errado que o girassol fazia... Mas também, era algo dentro de mim.`),
+
+p(`Nunca tive a oportunidade de ter um amor feliz. De poder amar e ser amada, sem medo ou sem me diminuir. Sempre amei sozinha ou fui deixada de lado... Ou sempre tive que me diminuir ou ficar zangada comigo mesma, por sempre criticarem a pessoa que sou por fora.`),
+
+p(`A idéia de que meu girassol realmente gostava de mim e me queria, não conseguia entrar na minha cabeça. Pois depois de todas às coisas difíceis que passei, e o que essas experiências me fizeram pensar e sentir sobre mim, pensava que não era merecedora ou que isso nunca ia acontecer comigo de verdade... Afinal, nunca aconteceu antes.`),
+
+p(`Era uma coisa boa, claro que sim. Mas, tinha medo de aceitar... Tinha medo de acreditar e meu girassol me deixar de lado de repente... Ou apenas me usar.`),
+
+p(`O fato de que ele não é um girassol muito demonstrativo também, só me deixou ainda mais confusa e triste. Pois, às vezes parecia que ele realmente gostava de mim, às vezes nem tanto. Não sabia ao certo o que sentir e isso me deixou com medo. Tinha medo de ficar feliz, mas a verdade ser outra e acabar ficando triste. Preferi apenas ficar triste, pois tenho medo de me sentir feliz...`),
+
+p(`Mas os meses foram passando, muitas coisas acontecendo, minha mente mudando... E aos poucos, fui me abrindo com meu girassol e entendendo seu lado também. E aí, às coisas foram ficando melhores de digerir.`),
+
+p(`Eu só sei que apenas tenho a agradecer, ao meu girassol. Pela companhia, pelo carinho, e por todas coisas que você me ensinou e me fez entender, mesmo que intencionalmente.`),
+
+p(`Você me ensinou, e ainda estou aprendendo, a aceitar mais o jeito das pessoas como elas são. Desde que elas não estejam fazendo mal a ninguém, não tem porquê querer molda - lás, temos nossas diferenças e está tudo bem. O importante é se gostarmos, respeitar - mos e nos aceitarmos como somos..`),
+
+p(`E também... Você me ensinou a amar. E a tentar ser uma pessoa melhor também, em todos os sentidos.`),
+
+
+p(`Estou passando por uma fase um pouco difícil em minha vida, mas você muitas vezes e me fez sorrir e foi o motivo de minha alegria. Obrigada pela companhia e por tudo que você já fez por mim. Você é muito especial para mim e eu realmente te amo muito...`),
+
+p(`Você é o girassol mais lindo que já conheci. E você fica lindo com qualquer cor, e de todos os jeitos também.`),
+
+p(`Sua personalidade é incrível, mesmo você sendo um pouco chato às vezes. Você é inteligente, é dedicado, é esforçado, é divertido e uma ótima companhia também.`),
+
+p(`Gosto de sua maneira de pensar e ver às coisas desse mundo. Sua voz é linda e você fala bem.`),
+
+p(`Basicamente, tudo em você é incrível. Mentalmente, exteriormente e fisicamente falando.`),
+
+p(`Obrigada por existir e ter entrado na minha vida... Você me ajuda a ficar muito melhor. E espero que lhe cause a mesma sensação também.`),
+p(`Te chamei de girassol, pois girassol muitas vezes está associado a felicidade. Felicidade está conectado a alegria, e foi isso que você me fez sentir desde que nós nos aproximamos.`),
+
+p(`Você também me deu um pouco do seu calor, da sua amizade, do seu amor, e de seu tempo também. Assim, retribui a você.`),
+
+p(`Não sei o que vai acontecer daqui para a frente, como às coisas vão estar... Mas hoje, posso dizer com certeza que te amo muito e quero te amar ainda mais. Quero te encontrar, e ter presente em minha vida por bastante tempo. Quero dividir ela com você... (Tudo isso se você quiser também, é claro).`),
+
+p(`Enfim... Eu te amo, meu girassol. Muito obrigada por tudo. Você é incrível e lhe desejo tudo de bom. E estou aqui para o que precisar também.`),
+
+p(`Antes de encerrar, apenas algo que li e que acho importante você ler também:`),
+
+p(`"Girassol, você gira o sol que tenho e em sorriso, aquece o sol que sou. Os girassóis me ensinaram a ter esperança, pois no fim das contas, o sol sempre volta para nos iluminar. É necessário coragem para ser como um girassol, pois haverão dias sem luz. ... No futuro sempre haverá luz, se você viver como um girassol. "`),
+
+p(`É isso... Eu te amo.`),
+
+p(`E ter conhecido esse girassol... Foi uma das melhores coisas que já aconteceram`));
+
+}
+
+ReactDOM.render( e(App), document.body);
